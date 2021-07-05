7ecacff72f4ff0409334b95ad7f1c213a52d9372
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-07-05 13:00:40 -0600

    test db con setup, need to setup db con to return promise

--- test/test.js
+++ test/test.js
@@ -1,12 +1,27 @@
-const assert = require("assert");
+const assert  = require( "assert" );
+const dbUtils = require( "./utils/db" );
+const db      = require( "../lib/db" );
 
-describe("First Test", function() {
+/**
+ * TODO modify the db con so that it can be awaited.
+ * This will require a change to the app.js file as we will want to await the 
+ * db connection instead of just starting the connection before the app is 
+ * setup.
+ */
 
-  describe("#indexof()", function() {
+describe( "First Test", function() {
 
-    it("should return -1 when the value is not present", function() {
+	beforeEach( async () => {
+		await dbUtils.clean();
 
-      assert.equal( [ 1, 2, 3 ].indexOf(4), -1 );
+		console.log( 'cleaned db' );
+	});
+
+  describe( "#indexof()", function() {
+
+    it( "should return -1 when the value is not present", function() {
+
+      assert.equal( [ 1, 2, 3 ].indexOf( 4 ), -1 );
 
     });
 
