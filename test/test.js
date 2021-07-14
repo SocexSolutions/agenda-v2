7c0ab93bec0521a407e745a63671d44e8d097b2b
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-07-13 21:22:52 -0600

    fixing weird spacing issue

--- test/test.js
+++ test/test.js
@@ -1,36 +1,35 @@
-const assert  = require( "assert" );
-const http    = require( "http" );
-const dbUtils = require( "./utils/db" );
-const db      = require( "../lib/db" );
+const assert  = require("assert");
+const http    = require("http");
+const dbUtils = require("./utils/db");
+const db      = require("../lib/db");
 
-describe( "First Test", function() {
+describe("First Test", function() {
 
-	before( async( ) => {
-		await db.connect();
-	});
+  before(async() => {
+    await db.connect();
+  });
 
-	beforeEach( async( ) => {
-		await dbUtils.clean();
-	});
+  beforeEach(async() => {
+    await dbUtils.clean();
+  });
 
-  describe( "#indexof()", function( ) {
+  describe("#indexof()", function() {
 
-    it( "should return -1 when the value is not present", async( ) => {
+    it("should return -1 when the value is not present", async() => {
 
-			const options = {
-				hostname: 'localhost',
-				port: '5000',
-				path: '/user/register',
-				method: 'POST',
-			}
+      const options = {
+        hostname: "localhost",
+        port: "5000",
+        path: "/user/register",
+        method: "POST",
+      };
 
-			const req = http.request( options, ( res ) => {
-				console.log( `STATUS: ${ res.statusCode }` );
+      const req = http.request(options, (res) => {
+        console.log(`STATUS: ${ res.statusCode }`);
 
-				throw new Error( res );
-			} );
+        throw new Error(res);
+      });
 
-		
     });
 
   });
