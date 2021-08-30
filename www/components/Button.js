a248a067422f2635f9fc661788eddda6bae0243e
David Oligney <davidoligney@gmail.com>
2021-08-30 15:30:13 -0600

    nav bar changing based on if user signed in

--- www/components/Button.js
+++ www/components/Button.js
@@ -1,95 +1,110 @@
-import PropTypes from "prop-types";
-import { render } from "react-dom";
-import styles from "../styles/Button.module.css";
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+import styles from '../styles/Button.module.css';
 
-import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
-import CheckBoxOutlinedIcon from "@material-ui/icons/Checkboxoutlined"
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import CheckBoxOutlinedIcon from '@material-ui/icons/Checkboxoutlined';
 import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import AddToPhotosOutlinedIcon from '@material-ui/icons/AddToPhotosOutlined';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const Button = ({ icon, text, size, onClick, varient, stretch }) => {
   Button.propTypes = {
-    onClick: PropTypes.func,
-    stretch: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.bool
-    ]),
-    varient: PropTypes.string,
+    icon: PropTypes.string,
     text: PropTypes.string,
     size: PropTypes.string,
-    icon: PropTypes.string,
+    onClick: PropTypes.func,
+    varient: PropTypes.string,
+    stretch: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   };
 
   Button.defaultProps = {
     stretch: false,
-    text: "Button Text",
-    size: "medium",
-    varient: "primary",
-    icon: "",
+    text: ' ',
+    size: 'medium',
+    varient: 'primary',
+    icon: '',
   };
 
   let className = styles.btn;
   // size cases
   switch (size) {
-    case "small":
-      className += " " + styles.small;
+    case 'small':
+      className += ' ' + styles.small;
       break;
-    case "medium":
-      className += " " + styles.medium;
+    case 'medium':
+      className += ' ' + styles.medium;
       break;
-    case "large":
-      className += " " + styles.large;
+    case 'large':
+      className += ' ' + styles.large;
       break;
-    case "xl":
-      className += " " + styles.xl;
+    case 'xl':
+      className += ' ' + styles.xl;
       break;
   }
   //varient case
   switch (varient) {
-    case "primary":
-      className += " " + styles.primary;
+    case 'primary':
+      className += ' ' + styles.primary;
       break;
-    case "secondary":
-      className += " " + styles.secondary;
+    case 'secondary':
+      className += ' ' + styles.secondary;
       break;
-    case "danger":
-      className += " " + styles.danger;
+    case 'danger':
+      className += ' ' + styles.danger;
       break;
-    case "disabled":
-      className += " " + styles.disabled;
+    case 'disabled':
+      className += ' ' + styles.disabled;
       break;
-    case "menu":
-      className += " " + styles.menu;
+    case 'menu':
+      className += ' ' + styles.menu;
       break;
-    case "topic":
-      className += " " + styles.topic;
+    case 'topic':
+      className += ' ' + styles.topic;
+      break;
+    case 'icon':
+      className += ' ' + styles.icon;
       break;
   }
   //stretch case
   switch (stretch) {
-    case "medium":
-      className += " " + styles.stretchMedium;
+    case 'medium':
+      className += ' ' + styles.stretchMedium;
       break;
-    case "wide":
-      className += " " + styles.stretchWide;
+    case 'wide':
+      className += ' ' + styles.stretchWide;
       break;
     case false:
       break;
   }
   //icons
   switch (icon) {
-    case "doubleArrow":
-      icon = <DoubleArrowIcon style={{ color: "var(--agendaPurple)" }} />;
+    case 'doubleArrow':
+      icon = <DoubleArrowIcon style={{ color: 'var(--agendaPurple)' }} />;
       break;
-    case "checkBox":
-      icon = <CheckBoxOutlinedIcon style={{ color: "var(--agendaGreen)" }} />;
+    case 'checkBox':
+      icon = <CheckBoxOutlinedIcon style={{ color: 'var(--agendaGreen)' }} />;
       break;
-    case "cancel":
-      icon = <CancelOutlinedIcon style={{ color: "var(--danger)" }} />;
+    case 'cancel':
+      icon = <CancelOutlinedIcon style={{ color: 'var(--danger)' }} />;
       break;
-    case "arrow":
-      icon = <ArrowForwardIosIcon style={{ fontSize: "medium", color: "var(--textBlue)"}}/>;
+    case 'arrow':
+      icon = (
+        <ArrowForwardIosIcon
+          style={{ fontSize: 'medium', color: 'var(--textBlue)' }}
+        />
+      );
+      break;
+    case 'home':
+      icon = <HomeOutlinedIcon />;
+      break;
+    case 'addicon':
+      icon = <AddToPhotosOutlinedIcon />;
+      break;
+    case 'person':
+      icon = <AccountCircleIcon />;
       break;
   }
 
--- www/components/Nav.js
+++ www/components/Nav.js
@@ -1,35 +1,54 @@
-import Link from "next/link";
+import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import Button from "./Button";
-import { userRegister } from "../store/features/user/userSlice";
+import Button from './Button';
+import { userRegister } from '../store/features/user/userSlice';
+import styles from '../styles/Nav.module.css';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import PersonIcon from '@material-ui/icons/Person';
 
-import styles from "../styles/Nav.module.css";
 // this should probably go somewhere else so we can reuse it
-const selectUser = state => state.user;
+const selectUser = (state) => state.user;
 
 const Nav = () => {
+  const user = useSelector(selectUser);
+  console.log(user);
 
-  const user = useSelector( selectUser );
-  console.log( user );
+  if (user.token === null) {
+    return (
+      <>
+        <nav className={styles.nav}>
+          <div className={styles.Agenda}>
+            <Link href='/'>Agenda</Link>
+          </div>
+          <div className={styles.login}>
+            <Link href='/login'>
+              <Button varient='secondary' text='Login' />
+            </Link>
+            <Link href='/register'>
+              <Button text='Sign Up' />
+            </Link>
+          </div>
+        </nav>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <nav className={styles.nav}>
+          <Link href='/' passHref>
+            <Button icon='home' varient='icon' />
+          </Link>
 
-  return (
-    <>
-      <nav className={styles.nav}>
-        <h1>{ user.username }</h1>
-        <ul className={styles.Agenda}>
-          <li>
-            <Link href="/">Agenda</Link>
-          </li>
-        </ul>
-        <div className={styles.login}>
-          <Link href="/register">Register</Link>
-          <Button varient="secondary" text="Login" />
-          <Button text="Sign Up" />
-        </div>
-      </nav>
-    </>
-  );
+          <div className={styles.navcentered}>
+            <Link href='/messageExample' passHref>
+              <Button icon='addicon' text='create' varient='secondary' />
+            </Link>
+          </div>
+          <Button icon='person' varient='icon' size='small' />
+        </nav>
+      </>
+    );
+  }
 };
-
 export default Nav;
