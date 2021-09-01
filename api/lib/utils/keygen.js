253222e9321f4f2ed889b2d1c3c6f4523605bfd8
zbarnz <zbarnz@yahoo.com>
2021-09-01 12:53:41 -0600

    a loading screen icon with 3 sizes

--- api/lib/utils/keygen.js
+++ api/lib/utils/keygen.js
@@ -1,26 +1,26 @@
-const crypto = require( "crypto" );
-const fs     = require( "fs" );
+const crypto = require("crypto");
+const fs = require("fs");
 
 /**
  * Generates a public private key pair to be used by the app.
  */
 function generateKeyPair() {
-  console.log( "generating keys...\n" );
+  console.log("generating keys...\n");
 
-  const keyPair = crypto.generateKeyPairSync( "rsa", {
+  const keyPair = crypto.generateKeyPairSync("rsa", {
     modulusLength: 4096, // length of key size (4096 is standard)
     publicKeyEncoding: {
       type: "pkcs1", // public key crypography standards 1
-      format: "pem"
+      format: "pem",
     },
     privateKeyEncoding: {
       type: "pkcs1",
-      format: "pem"
-    }
+      format: "pem",
+    },
   });
 
-  fs.writeFileSync( __dirname + "/../id_rsa_pub.pem", keyPair.publicKey );
-  fs.writeFileSync( __dirname + "/../id_rsa_priv.pem", keyPair.privateKey );
+  fs.writeFileSync(__dirname + "/../id_rsa_pub.pem", keyPair.publicKey);
+  fs.writeFileSync(__dirname + "/../id_rsa_priv.pem", keyPair.privateKey);
 }
 
 generateKeyPair();
---
+++ www/components/LoadingIcon.js
@@ -0,0 +1,45 @@
+import styles from "../styles/LoadingIcon.module.css";
+import PropTypes from "prop-types";
+
+const LoadingIcon = ({size}) => {
+  switch(size){
+    case "small":
+      size = {width: "50px", height:"50px"}
+      break;
+    case "medium":
+      size = {width: "100px", height:"100px"}
+      break;
+    case "large":
+      size = {width: "150px", height:"150px"}
+      break;
+
+  }
+
+  return (
+    <div className={styles.container} style={size}>
+      <div id="circle" className={styles.circleOuter}>
+          <div id="hourHand" className={styles.hourHand}></div>
+          <div id="minuteHand" className={styles.minuteHand}></div>
+          <div className={styles.ticks}></div>
+          <div className={styles.ticks} style={{transform: "rotate(30deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(30deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(60deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(90deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(120deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(150deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(180deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(210deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(240deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(270deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(300deg) translate(0, -850%)"}}></div>
+          <div className={styles.ticks} style={{transform: "rotate(330deg) translate(0, -850%)"}}></div>
+      </div>
+    </div>
+  );
+};
+
+LoadingIcon.propTypes = {
+  size: PropTypes.string
+}
+
+export default LoadingIcon;
