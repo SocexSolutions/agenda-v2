17596d051729cf5789b19519b9689cfac16469d7
David Oligney <davidoligney@gmail.com>
2021-09-01 22:47:09 -0600

    did find all single quotes and replaced with doubles. your welcome now give me my darn commit

--- www/components/Button.js
+++ www/components/Button.js
@@ -1,14 +1,14 @@
-import PropTypes from 'prop-types';
-import { render } from 'react-dom';
-import styles from '../styles/Button.module.css';
+import PropTypes from "prop-types";
+import { render } from "react-dom";
+import styles from "../styles/Button.module.css";
 
-import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
-import CheckBoxOutlinedIcon from '@material-ui/icons/Checkboxoutlined';
-import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
-import AddToPhotosOutlinedIcon from '@material-ui/icons/AddToPhotosOutlined';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+import CheckBoxOutlinedIcon from "@material-ui/icons/Checkboxoutlined";
+import CancelOutlinedIcon from "@material-ui/icons/CancelOutlined";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
+import AddToPhotosOutlinedIcon from "@material-ui/icons/AddToPhotosOutlined";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 const Button = ({ icon, text, size, onClick, varient, stretch }) => {
   Button.propTypes = {
@@ -22,88 +22,88 @@ const Button = ({ icon, text, size, onClick, varient, stretch }) => {
 
   Button.defaultProps = {
     stretch: false,
-    text: ' ',
-    size: 'medium',
-    varient: 'primary',
-    icon: '',
+    text: " ",
+    size: "medium",
+    varient: "primary",
+    icon: "",
   };
 
   let className = styles.btn;
   // size cases
   switch (size) {
-    case 'small':
-      className += ' ' + styles.small;
+    case "small":
+      className += " " + styles.small;
       break;
-    case 'medium':
-      className += ' ' + styles.medium;
+    case "medium":
+      className += " " + styles.medium;
       break;
-    case 'large':
-      className += ' ' + styles.large;
+    case "large":
+      className += " " + styles.large;
       break;
-    case 'xl':
-      className += ' ' + styles.xl;
+    case "xl":
+      className += " " + styles.xl;
       break;
   }
   //varient case
   switch (varient) {
-    case 'primary':
-      className += ' ' + styles.primary;
+    case "primary":
+      className += " " + styles.primary;
       break;
-    case 'secondary':
-      className += ' ' + styles.secondary;
+    case "secondary":
+      className += " " + styles.secondary;
       break;
-    case 'danger':
-      className += ' ' + styles.danger;
+    case "danger":
+      className += " " + styles.danger;
       break;
-    case 'disabled':
-      className += ' ' + styles.disabled;
+    case "disabled":
+      className += " " + styles.disabled;
       break;
-    case 'menu':
-      className += ' ' + styles.menu;
+    case "menu":
+      className += " " + styles.menu;
       break;
-    case 'topic':
-      className += ' ' + styles.topic;
+    case "topic":
+      className += " " + styles.topic;
       break;
-    case 'icon':
-      className += ' ' + styles.icon;
+    case "icon":
+      className += " " + styles.icon;
       break;
   }
   //stretch case
   switch (stretch) {
-    case 'medium':
-      className += ' ' + styles.stretchMedium;
+    case "medium":
+      className += " " + styles.stretchMedium;
       break;
-    case 'wide':
-      className += ' ' + styles.stretchWide;
+    case "wide":
+      className += " " + styles.stretchWide;
       break;
     case false:
       break;
   }
   //icons
   switch (icon) {
-    case 'doubleArrow':
-      icon = <DoubleArrowIcon style={{ color: 'var(--agendaPurple)' }} />;
+    case "doubleArrow":
+      icon = <DoubleArrowIcon style={{ color: "var(--agendaPurple)" }} />;
       break;
-    case 'checkBox':
-      icon = <CheckBoxOutlinedIcon style={{ color: 'var(--agendaGreen)' }} />;
+    case "checkBox":
+      icon = <CheckBoxOutlinedIcon style={{ color: "var(--agendaGreen)" }} />;
       break;
-    case 'cancel':
-      icon = <CancelOutlinedIcon style={{ color: 'var(--danger)' }} />;
+    case "cancel":
+      icon = <CancelOutlinedIcon style={{ color: "var(--danger)" }} />;
       break;
-    case 'arrow':
+    case "arrow":
       icon = (
         <ArrowForwardIosIcon
-          style={{ fontSize: 'medium', color: 'var(--textBlue)' }}
+          style={{ fontSize: "medium", color: "var(--textBlue)" }}
         />
       );
       break;
-    case 'home':
+    case "home":
       icon = <HomeOutlinedIcon />;
       break;
-    case 'addicon':
+    case "addicon":
       icon = <AddToPhotosOutlinedIcon />;
       break;
-    case 'person':
+    case "person":
       icon = <AccountCircleIcon />;
       break;
   }
