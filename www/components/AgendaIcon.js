b62b9d7c3ddaf2f147c784fb2d652f78a0bb4008
David Oligney <davidoligney@gmail.com>
2021-09-05 17:58:15 -0600

    changed everything but color for now

--- www/components/AgendaIcon.js
+++ www/components/AgendaIcon.js
@@ -1,17 +1,18 @@
-import CalendarTodayRoundedIcon from '@material-ui/icons/CalendarTodayRounded';
-import AccessAlarmRoundedIcon from '@material-ui/icons/AccessAlarmRounded';
-import styles from "../styles/AgendaIcon.module.css"
+import CalendarTodayRoundedIcon from "@material-ui/icons/CalendarTodayRounded";
+import AccessAlarmRoundedIcon from "@material-ui/icons/AccessAlarmRounded";
+import styles from "../styles/AgendaIcon.module.css";
+
 function AgendaIcon() {
-    return (
-        <div className={styles.contain}>
-            <CalendarTodayRoundedIcon style={{fontSize: 35,
-            color: "var(--agendaSecondary)"}}/>
-            <div className={styles.check}>
-                <AccessAlarmRoundedIcon style={{fontSize: 17, 
-                    color: "var(--agendaSecondary)"}}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <CalendarTodayRoundedIcon style={{ fontSize: 35,
+        color: "var(--agendaSecondary)" }}/>
+      <div className={styles.check}>
+        <AccessAlarmRoundedIcon style={{ fontSize: 17,
+          color: "var(--agendaSecondary)" }}/>
+      </div>
+    </div>
+  );
 }
 
-export default AgendaIcon
+export default AgendaIcon;
