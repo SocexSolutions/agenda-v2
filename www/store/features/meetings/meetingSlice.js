6da1937a8f2610937d0b5a7b5ec95ad14a250951
thudsonbu <thudson@starry.com>
2021-09-12 13:46:27 -0600

    Add meeting slice with fetchMeeting action.

---
+++ www/store/features/meetings/meetingSlice.js
@@ -0,0 +1,36 @@
+import client from "../../client";
+
+const initialState = {
+  openMeeting: {},
+  userMeetings: []
+};
+
+const reducer = ( state = initialState, action ) => {
+  switch ( action.type ) {
+
+  case "meeting/fetch":
+    return { ...state, openMeeting: action.payload };
+  }
+};
+
+export const fetchMeeting = ( meeting_id ) => {
+  return async function fetchMeeting( dispatch, getState ) {
+    try {
+      const { data } = await client.get( `meeting/${ meeting_id }` );
+
+      dispatch(
+        {
+          type: "meeting/fetch",
+          payload: {
+            ...data
+          }
+        }
+      );
+    } catch ( error ) {
+
+      console.error( error.message );
+    }
+  };
+};
+
+export default reducer;
