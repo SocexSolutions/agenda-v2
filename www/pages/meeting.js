1aaa7e0c753a999e1e6c855320d172bc9cba6d60
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-09-14 21:32:26 -0600

    Meeting form methods.
    Co-authored-by: zbarnz <zbarnz@yahoo.com>
    Co-authored-by: dahvd <davidoligney@gmail.com>

--- www/pages/meeting.js
+++ www/pages/meeting.js
@@ -1,16 +1,98 @@
-import Participant from "../components/Participant";
+import { Component } from "react";
 import MeetingNameDate from "../components/MeetingNameDate";
 import MeetingTopics from "../components/MeetingTopics";
 import Attendies from "../components/Attendies";
 
-function meeting() {
-  return (
-    <div>
-      <MeetingNameDate />
-      <MeetingTopics />
-      <Attendies />
-    </div>
-  );
+class Meeting extends Component {
+  constructor( props ) {
+    super( props );
+
+    this.meeting = {};
+    this.topics = [];
+    this.attendies = [];
+    this.editing = false;
+
+    this.addTopic = this.addTopic.bind( this );
+    this.deleteTopic = this.deleteTopic.bind( this );
+    this.setMeetingName = this.setMeetingName.bind( this );
+    this.setMeetingDate = this.setMeetingDate.bind( this );
+    this.addParticipant = this.addParticipant.bind( this );
+    this.deleteParticipant = this.deleteParticipant.bind( this );
+    this.setImportance = this.setImportance.bind( this );
+  }
+
+  addTopic( topicName ) {
+    const newTopics = this.topics;
+
+    newTopics.push( topicName );
+
+    this.setState({ ...this.state, topics: newTopics });
+  }
+
+  deleteTopic( deleteTopic ) {
+    const newTopics = this.topics.filter( ( topic ) => {
+      return topic !== deleteTopic;
+    });
+
+    this.setState({ ...this.state, topics: newTopics });
+  }
+
+  setMeetingName( name ) {
+    this.setState({
+      ...this.state,
+      meeting: { ...this.meeting, name },
+    });
+  }
+
+  setMeetingDate( date ) {
+    this.setState({
+      ...this.state,
+      meeting: { ...this.meeting, date },
+    });
+  }
+
+  addParticipant( participant ) {
+    const newParticipant = this.attendies;
+
+    newParticipant.push( participant );
+
+    this.setState({ ...this.state, attendies: newParticipant });
+  }
+
+  deleteParticipant( deleteParticipant ) {
+    const newParticipants = this.topics.filter( ( participant ) => {
+      return participant === deleteParticipant;
+    });
+
+    this.setState({ ...this.state, participant: newParticipants });
+  }
+
+  setImportance( flag ) {
+    this.setState({
+      ...this.state,
+      meeting: { ...this.meeting, importance: flag },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <MeetingNameDate
+          setImportance={this.setImportance}
+          setMeetingName={this.setMeetingName}
+          setMeetingDate={this.setMeetingDate}
+        />
+        <MeetingTopics
+          addTopic={this.addTopic}
+          deleteTopic={this.deleteTopic}
+        />
+        <Attendies
+          addParticipant={this.addParticipant}
+          deleteParticipant={this.deleteParticipant}
+        />
+      </div>
+    );
+  }
 }
 
-export default meeting;
+export default Meeting;
