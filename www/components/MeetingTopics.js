3d4daf49d7c1a333cc4dd57c16656366cd0efc12
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-09-21 20:17:19 -0600

    wip

--- www/components/MeetingTopics.js
+++ www/components/MeetingTopics.js
@@ -2,17 +2,20 @@ import styles from "../styles/MeetingTopics.module.css";
 import Chip from "./Chip";
 import Input from "./Input";
 
-function MeetingTopics() {
+function MeetingTopics( props ) {
   return (
     <div className={styles.container}>
       <h2 className={styles.topicsTitle}>Topics</h2>
       <div className={styles.topics}>
-        <Chip text="Topic 1"/>
-        <Chip text="Topic 2"/>
-        <Chip text="Topic 3"/>
+        <Chip text={ props.topics ? props.topics[0] : ""} />
+        <Chip text="Topic 2" />
+        <Chip text="Topic 3" />
       </div>
       <p>Add Topic</p>
-      <Input placeholder="Topic"/>
+      <Input
+        placeholder="Topic"
+        onChange={() => alert( "NOTICE: You are editing your topics" )}
+      />
     </div>
   );
 }
