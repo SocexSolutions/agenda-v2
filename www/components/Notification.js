f149665f6c840debc1ff01150c02704ee755b34c
zbarnz <zbarnz@yahoo.com>
2021-10-03 16:48:13 -0600

    Added box shadow, error and success varients, and updated popup location to the top of the page

--- www/components/Notification.js
+++ www/components/Notification.js
@@ -6,12 +6,26 @@ import { useState } from "react";
 
 import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
 
-const Notification = ({ message }) => {
+const Notification = ({ message, error }) => {
   const [ close, setClose ] = useState( false );
 
+  if( error == true ) {
+    error = "Error:";
+  }
+  else {
+    error == "Success:";
+  }
+
   return (
-    <div className={classNames( styles.container, close && styles.closePressed )}>
-      <div onClick={() => setClose( !close )}><CancelPresentationIcon className={styles.close} /></div>
+    <div
+      className={classNames( styles.container, close && styles.closePressed )}
+    >
+      <div
+        onClick={() =>
+          setClose( !close )}><CancelPresentationIcon className={styles.close}
+        />
+      </div>
+      <h1>{error}</h1>
       <p>{message}</p>
       <div className={styles.loadingBar}></div>
     </div>
@@ -19,11 +33,13 @@ const Notification = ({ message }) => {
 };
 
 Notification.propTypes = {
-  message: PropTypes.string
+  message: PropTypes.string,
+  error: PropTypes.bool
 };
 
 Notification.defaultProps = {
-  message: "Something went wrong"
+  message: "Something went wrong",
+  error: true
 };
 
 export default Notification;
