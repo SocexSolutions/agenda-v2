70ea94f0822bdaa19f539fad02a719eba30c4088
thudsonbu <thudson@starry.com>
2021-10-31 20:42:04 -0600

    Added additional logging to make debugging easier later on.

--- api/lib/controllers/topic.js
+++ api/lib/controllers/topic.js
@@ -1,14 +1,19 @@
-const Topic = require( "../models/topic" );
+const logger = require( "@starryinternet/jobi" );
+const Topic  = require( "../models/topic" );
 
 module.exports = {
   create: async( req, res ) => {
+    logger.debug( "Creating topic with:  " + JSON.stringify( req.body ) );
     const { name, meeting_id, likes } = req.body;
 
     try {
       const topic = await Topic.create({ name, meeting_id, likes });
 
+      logger.debug( "Topic created: " + JSON.stringify( topic ) );
       res.status( 201 ).send( topic );
     } catch ( error ) {
+
+      logger.error( "Error creating topic: " + error.message );
       res.status( 500 ).send( error.message );
     }
   },
