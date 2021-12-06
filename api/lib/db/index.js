42e94f9a97458e54ad9ffd8ad27d8c676c4774bb
thudsonbu <thudson@starry.com>
2021-12-05 18:30:22 -0700

    Upgrade agenda to node 16.13.1 and update dockerfiles.

--- api/lib/db/index.js
+++ api/lib/db/index.js
@@ -1,19 +1,22 @@
-const logger   = require( "@starryinternet/jobi" );
-const mongoose = require( "mongoose" );
+const jobi     = require('@starryinternet/jobi');
+const mongoose = require('mongoose');
 
-let db  = {};
+const db  = {};
 
 db.connect = async() => {
   try {
-    let dbConStr = "mongodb://host.docker.internal:27017/agenda";
+    // because docker compose create a docker network, we use this internal
+    // network by default, but for local dev we use localhost instead
 
-    if ( process.env.NODE_ENV === "test" ) {
-      dbConStr = "mongodb://localhost:27017/agenda-test";
-    } else if ( process.env.NODE_ENV === "dev" ) {
-      dbConStr = "mongodb://localhost:27017/agenda-dev";
+    let dbConStr = 'mongodb://host.docker.internal:27017/agenda';
+
+    if ( process.env.NODE_ENV === 'test' ) {
+      dbConStr = 'mongodb://localhost:27017/agenda-test';
+    } else if ( process.env.NODE_ENV === 'dev' ) {
+      dbConStr = 'mongodb://localhost:27017/agenda-dev';
     }
 
-    logger.info( "using dbConStr " + dbConStr );
+    jobi.info( 'using dbConStr ' + dbConStr );
 
     await mongoose.connect( dbConStr, {
       useNewUrlParser: true,
@@ -21,16 +24,16 @@ db.connect = async() => {
       useCreateIndex: true
     });
 
-    logger.info( "database connected" );
+    jobi.info('database connected');
 
   } catch ( error ) {
 
-    logger.error( "database connection failed: " + error.message );
+    jobi.error( 'database connection failed: ' + error.message );
   }
 };
 
 db.disconnect = async() => {
-  logger.info( "disconnecting database" );
+  jobi.info('disconnecting database');
 
   try {
     await mongoose.disconnect();
@@ -40,7 +43,7 @@ db.disconnect = async() => {
     console.error( err );
   }
 
-  logger.info( "database disconnected" );
+  jobi.info('database disconnected');
 };
 
 module.exports = db;
