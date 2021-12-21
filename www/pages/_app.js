f8eb02b2be03f9457b11a31fb28df6f2a13fc6ce
thudsonbu <thudson@starry.com>
2021-12-20 20:39:48 -0700

    Add authorization interceptor to client.

--- www/pages/_app.js
+++ www/pages/_app.js
@@ -3,7 +3,6 @@ import { Provider }    from 'react-redux';
 import { useStore }    from '../store/store';
 import { useEffect }   from 'react';
 import { userRefresh } from '../store/features/user/userSlice';
-import parseCookie     from '../utils/parseCookie';
 
 import '../styles/globals.css';
 
@@ -11,13 +10,9 @@ function MyApp({ Component, pageProps }) {
   const store = useStore({});
 
   useEffect( () => {
-    const token = parseCookie( document.cookie, 'agenda-auth' );
-
-    if ( token ) {
-      store.dispatch(
-        userRefresh( token )
-      );
-    }
+    store.dispatch(
+      userRefresh()
+    );
   });
 
   return (
