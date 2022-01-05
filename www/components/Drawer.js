3c751124336b2a3fab0458a7a7d0b26e3c58e155
zbarnz <zbarnz@yahoo.com>
2022-01-04 21:36:05 -0700

    changed up arrow look

--- www/components/Drawer.js
+++ www/components/Drawer.js
@@ -1,7 +1,7 @@
 import styles from "../styles/Drawer.module.css";
 import PropTypes from "prop-types";
 import Button from "../components/Button";
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIosTwoTone';
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIosTwoTone";
 
 
 import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
@@ -11,53 +11,58 @@ import CancelOutlinedIcon from "@material-ui/icons/CancelOutlined";
 const Drawer = ({ drawerOpen, setDrawerOpen }) => {
 
   return (
-    <div 
-      className={`${ styles.container } ${ !drawerOpen && styles.closed }`}
-    >
-      <h2>Socnet</h2>
-      <Button
-        text='Upcoming'
-        size='large'
-        stretch='wide'
-        varient='menu'
-        icon={<DoubleArrowIcon style={{ color: "var(--agendaPurple)" }} /> }
-      />
-      <Button
-        text='Voting'
-        size='large'
-        stretch='wide'
-        varient='menu'
-        icon={<CheckBoxOutlinedIcon style={{ color: "var(--agendaGreen)" }} />}
-      />
-      <Button
-        text='Completed'
-        size='large'
-        stretch='wide'
-        varient='menu'
-        icon={<CancelOutlinedIcon style={{ color: "var(--danger)" }} />}
-      />
-      <div 
-        className={styles.arrow} 
-        onClick={() => setDrawerOpen(!drawerOpen)}
+    <div className={styles.godContainer}>
+      <div
+        className={`${ styles.container } ${ !drawerOpen && styles.closed }`}
       >
-        <div 
-          className={drawerOpen ? styles.arrowClose : styles.arrowOpen} 
-        >
-          <ArrowForwardIosIcon />
+        <h2>Socnet</h2>
+        <Button
+          text='Upcoming'
+          size='large'
+          stretch='wide'
+          varient='menu'
+          icon={<DoubleArrowIcon style={{ color: "var(--agendaPurple)" }} /> }
+        />
+        <Button
+          text='Voting'
+          size='large'
+          stretch='wide'
+          varient='menu'
+          icon={<CheckBoxOutlinedIcon style={{ color: "var(--agendaGreen)" }} />}
+        />
+        <Button
+          text='Completed'
+          size='large'
+          stretch='wide'
+          varient='menu'
+          icon={<CancelOutlinedIcon style={{ color: "var(--danger)" }} />}
+        />
+
+      </div>
+      <div
+        className={styles.arrowContainer}
+        onClick={() => setDrawerOpen( !drawerOpen )}
+      >
+        <div className={styles.arrow}>
+          <div
+            className={drawerOpen ? styles.arrowClose : styles.arrowOpen}
+          >
+            <ArrowForwardIosIcon />
+          </div>
         </div>
       </div>
     </div>
   );
-};  
+};
 
 Drawer.propTypes = {
   drawerStatus: PropTypes.bool,
-  setDrawerOpen: PropTypes.func,
-}
+  setDrawerOpen: PropTypes.func
+};
 
 Drawer.defaultProps = {
-  drawerOpen: true,
-  // setDrawerOpen: 
+  drawerOpen: true
+  // setDrawerOpen:
 };
 
 export default Drawer;
