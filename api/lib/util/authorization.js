f64bde9dbc86fcc923390cc16486ac32a8614d0b
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-05-29 10:45:30 -0600

    refactor check_participant to support participants as subjects and add check_owner util

---
+++ api/lib/util/authorization.js
@@ -0,0 +1,75 @@
+const Participant = require('../models/participant');
+const Meeting     = require('../models/meeting');
+const mongoose    = require('mongoose');
+
+/**
+ * Check if a user or participant is a participant or owner of a meeting
+ *
+ * @param {ObjectId} meeting_id
+ * @param {Object} credentials
+ *
+ * @typedef {Object} AuthorizationResult
+ * @property {Boolean} authorized - user is authorized
+ * @property {Meeting} [meeting] - meeting returned for utility
+ *
+ * @returns {Promise<AuthorizationResult>}
+ */
+module.exports.check_participant = async( meeting_id, credentials ) => {
+  const { user, participant } = credentials;
+
+  if ( participant ) {
+    if ( participant.meeting_id.toString() === meeting_id.toString() ) {
+      const meeting = await Meeting.findOne({ _id: meeting_id });
+      return { authorized: true, meeting };
+    }
+
+    return { authorized: false };
+  }
+
+  const [ participant_res, meeting_res ] = await Promise.allSettled([
+    Participant.findOne({ meeting_id, email: user.email }),
+    Meeting.findOne({ _id: meeting_id })
+  ]);
+
+  const is_owner = meeting_res.status === 'fulfilled' &&
+    meeting_res.value.owner_id.toString() === user._id.toString();
+
+  const is_participant = participant_res.status === 'fulfilled' &&
+    participant_res.value;
+
+  if ( is_owner || is_participant ) {
+    return { authorized: true, meeting: meeting_res.value };
+  }
+
+  return { authorized: false };
+};
+
+/**
+ * Check if a user or participant is the owner of a document by comparing the
+ * owner_id with the subject id
+ *
+ * @param {ObjectId} _id - id of item to check
+ * @param {String} collection_name - collection of item to check
+ * @param {Object} credentials
+ *
+ * @typedef {Object} AuthorizationResult
+ * @property {Boolean} authorized - user or participant is authorized
+ * @property {Meeting} [document] - document owned by user returned for utility
+ *
+ * @returns {Promise<AuthorizationResult>}
+ */
+module.exports.check_owner = async( _id, collection_name, credentials ) => {
+  const { user, participant } = credentials;
+
+  const subject_id = user?._id || participant._id;
+
+  const collection = mongoose.connection.collection( collection_name );
+
+  const document = await collection.findOne({ _id });
+
+  if ( document.owner_id.toString() === subject_id.toString() ) {
+    return { authorized: true, document };
+  }
+
+  return { authorized: false };
+};
---
+++ api/test/tests/util/authorization.js
@@ -0,0 +1,140 @@
+const Meeting         = require('../../../lib/models/meeting');
+const Participant     = require('../../../lib/models/participant');
+const fakeMeeting     = require('../../fakes/meeting');
+const fakeParticipant = require('../../fakes/participant');
+const fakeUser        = require('../../fakes/user');
+const { clean }       = require('../../utils/db');
+const db              = require('../../../lib/db');
+const { assert }      = require('chai');
+const lib_rewire      = require('../../utils/lib-rewire');
+
+const module_path = 'lib/util/authorization';
+
+describe( module_path, () => {
+
+  beforeEach( async() => {
+    await db.connect();
+    await clean();
+
+    this.meeting          = fakeMeeting();
+    this.inserted_meeting = await Meeting.create( this.meeting );
+
+    this.participants = Array.from({ length: 3 }).map( () => {
+      return fakeParticipant({ meeting_id: this.inserted_meeting._id });
+    });
+
+    this.inserted_participants = await Participant.insertMany(
+      this.participants
+    );
+
+    this.module = lib_rewire( module_path );
+  });
+
+  after( async() => {
+    await db.disconnect();
+  });
+
+
+  describe( '#check_participant', () => {
+
+    it( 'should return authorized if user is participant', async() => {
+      const { authorized, meeting } = await this.module.check_participant(
+        this.inserted_meeting._id,
+        {
+          user: fakeUser({ _id: 'someid', email: this.participants[ 0 ].email })
+        }
+      );
+
+      assert.isTrue( authorized );
+      assert.equal( meeting.name, this.inserted_meeting.name );
+    });
+
+    it( 'should return authorized if user is meeting owner', async() => {
+      const { authorized, meeting } = await this.module.check_participant(
+        this.inserted_meeting._id,
+        { user: fakeUser({ _id: this.meeting.owner_id }) }
+      );
+
+      assert.isTrue( authorized );
+      assert.equal( meeting.name, this.inserted_meeting.name );
+    });
+
+    it( 'should return authorized if participant is participant', async() => {
+      const { authorized, meeting } = await this.module.check_participant(
+        this.inserted_meeting._id,
+        {
+          participant: fakeParticipant({
+            _id: this.participants[ 0 ]._id,
+            meeting_id: this.inserted_meeting._id
+          })
+        }
+      );
+
+      assert.isTrue( authorized );
+      assert.equal( meeting.name, this.inserted_meeting.name );
+    });
+
+    it( 'should return unauthorized if user is not participant', async() => {
+      const { authorized, meeting } = await this.module.check_participant(
+        this.inserted_meeting._id,
+        { user: fakeUser({ _id: 'some_id', email: 'some_email' }) }
+      );
+
+      assert.isFalse( authorized );
+      assert.notExists( meeting );
+    });
+
+    it( 'should return unauthorized if partic is not participant', async() => {
+      const { authorized, meeting } = await this.module.check_participant(
+        this.inserted_meeting._id,
+        {
+          participant: fakeParticipant({
+            _id: this.participants[ 0 ]._id
+          })
+        }
+      );
+
+      assert.isFalse( authorized );
+      assert.notExists( meeting );
+    });
+
+  });
+
+  describe( '#check_owner', () => {
+
+    it( 'should return authorized and doc if doc user is owner', async() => {
+      const { authorized, document } = await this.module.check_owner(
+        this.inserted_meeting._id,
+        'meetings',
+        { user: { _id: this.meeting.owner_id } }
+      );
+
+      assert.isTrue( authorized );
+      assert.equal( this.inserted_meeting.name, document.name );
+    });
+
+    it( 'should return authorized and doc if doc participant is owner', async() => {
+      const { authorized, document } = await this.module.check_owner(
+        this.inserted_meeting._id,
+        'meetings',
+        { participant: { _id: this.meeting.owner_id } }
+      );
+
+      assert.isTrue( authorized );
+      assert.equal( this.inserted_meeting.name, document.name );
+    });
+
+    it( 'should return unauthorized and no doc if not owner', async() => {
+      const { authorized, document } = await this.module.check_owner(
+        this.inserted_meeting._id,
+        'meetings',
+        { user: { _id: 'some_id' } }
+      );
+
+      assert.isFalse( authorized );
+      assert.notExists( document );
+    });
+
+  });
+
+});
