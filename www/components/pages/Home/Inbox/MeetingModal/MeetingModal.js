a5f3e4677bbf870c37a1423b45419babf36dfce5
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-10-09 18:22:51 -0400

    feat(www): add the ability to delete meetings

--- www/components/pages/Home/Inbox/MeetingModal/MeetingModal.js
+++ www/components/pages/Home/Inbox/MeetingModal/MeetingModal.js
@@ -3,12 +3,25 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
 import { useRouter } from 'next/router';
+import MeetingAPI from '../../../../../api/meeting';
 import styles from './MeetingModal.module.scss';
 
 export default function MeetingModal({ meeting, open, setOpen }) {
   const router = useRouter();
 
   const displayDate = new Date( meeting.date ).toLocaleDateString();
+
+  const onDelete = async() => {
+    if (
+      window.confirm(
+        'Are you sure you want to delete this meeting.' +
+        'This action cannot be undone. The meeting will no longer be visible ' +
+        'to anyone.'
+      )
+    ) {
+      await MeetingAPI.destroy( meeting._id );
+    }
+  };
 
   return (
     <Modal open={open} onClose={() => setOpen( false )}>
@@ -24,7 +37,7 @@ export default function MeetingModal({ meeting, open, setOpen }) {
             >
               <EditIcon />
             </IconButton>
-            <IconButton>
+            <IconButton onClick={() => onDelete()}>
               <DeleteIcon />
             </IconButton>
             <IconButton onClick={() => setOpen( false )}>
