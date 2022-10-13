a98f84fc802e3ee4451f066eb9919e04329aea0d
zbarnz <zbarnz@yahoo.com>
2022-10-12 21:36:16 -0600

    initilize dropdown

--- www/components/pages/Home/Inbox/Inbox.js
+++ www/components/pages/Home/Inbox/Inbox.js
@@ -1,9 +1,12 @@
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Autocomplete } from '@mui/material';
 import InboxRow from './InboxRow/InboxRow';
 import meetingAPI from '../../../../api/meeting';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import styles from './Inbox.module.scss';
+import classNames from 'classnames';
 import FilterListIcon from '@mui/icons-material/FilterList';
+import { height } from '@mui/system';
 
 /**
  * Table that displays users meetings
@@ -13,7 +16,9 @@ import FilterListIcon from '@mui/icons-material/FilterList';
 export default function Inbox({ meetings, refresh }) {
   const router = useRouter();
 
-  const lineItems = meetings.map((meeting) => {
+  const [ filtersOpen, setFiltersOpen ] = useState( false );
+
+  const lineItems = meetings.map( ( meeting ) => {
     return (
       <InboxRow
         meeting={meeting}
@@ -24,12 +29,31 @@ export default function Inbox({ meetings, refresh }) {
     );
   });
 
+  const top100Films = [
+    { label: 'Rimp', year: 1994 },
+    { label: 'Jogn', year: 1972 },
+    { label: 'Mercedes', year: 1974 },
+    { label: 'David', year: 2008 },
+    { label: 'Alison', year: 1957 },
+    { label: 'Zach', year: 1993 },
+    { label: 'Tom', year: 1994 }
+  ];
+
   if ( lineItems.length ) {
     return (
       <div className={styles.table}>
-        <div className={styles.filter_box}>
-          <TextField placeholder='Search' variant='standard' size='small'></TextField>
-          <Button endIcon={<FilterListIcon />} >Filters</ Button>
+        <div className={classNames( styles.filter_box, filtersOpen && styles.filter_box_open ) } >
+          <div className={styles.visible}>
+            <TextField placeholder='Search' variant='standard' size='small' />
+            <Button
+              onClick={() => setFiltersOpen( !filtersOpen )}
+              endIcon={<FilterListIcon />} >
+              Filters
+            </ Button>
+          </ div>
+          <div className={styles.hidden}>
+            <Autocomplete options={top100Films} sx={{ width: 200, height: 100 }} renderInput={( params ) => <TextField {...params} size='small' label="Owner" />} />
+          </div>
         </div>
         {lineItems}
       </div>
