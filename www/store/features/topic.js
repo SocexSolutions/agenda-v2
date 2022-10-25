670c2d699619496dc864f4133954f45078e72870
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-10-25 14:03:56 -0400

    feat(www): add support for action items and takeaways to topic slice

--- www/store/features/topic.js
+++ www/store/features/topic.js
@@ -8,7 +8,11 @@ const topicsSlice = createSlice({
   initialState: {},
   reducers: {
     create: (state, action) => {
-      state[action.payload._id] = action.payload;
+      state[action.payload._id] = {
+        takeaways: [],
+        actionItems: [],
+        ...action.payload,
+      };
     },
     update: (state, action) => {
       const { _id } = action.payload;
@@ -22,38 +26,72 @@ const topicsSlice = createSlice({
     updateMany: (state, action) => {
       action.payload.forEach((topic) => {
         // merge to avoid overwriting action items or takeaways
-        state[topic._id] = { ...state[topic._id], ...topic };
+        state[topic._id] = {
+          takeaways: [],
+          actionItems: [],
+          ...state[topic._id],
+          ...topic,
+        };
       });
     },
-    updateTakeaways: (state, action) => {
+    setTakeaways: (state, action) => {
       const { _id, takeawayIds } = action.payload;
 
       state[_id].takeaways = takeawayIds;
     },
+    createTakeaway: (state, action) => {
+      const { _id, takeawayId } = action.payload;
+
+      state[_id].takeaways = state[_id].takeaways.push(takeawayId);
+    },
+    deleteTakeaway: (state, action) => {
+      const { _id, takeawayId } = action.payload;
+
+      state[_id].takeaways = state[_id].takeaways.filter(
+        (t) => t !== takeawayId
+      );
+    },
+    setActionItems: (state, action) => {
+      const { _id, actionItemIds } = action.payload;
+
+      state[_id].actionItems = actionItemIds;
+    },
+    createActionItem: (state, action) => {
+      const { _id, actionItemId } = action.payload;
+
+      state[_id].actionItems = state[_id].actionItems.push(actionItemId);
+    },
+    deleteActionItem: (state, action) => {
+      const { _id, actionItemId } = action.payload;
+
+      state[_id].actionItems = state[_id].actionItems.filter(
+        (t) => t !== actionItemId
+      );
+    },
   },
 });
 
-export const createTopic = (topic) => {
+export const create = (topic) => {
   return async function create() {
-    const newTopic = await topicApi.create(topic);
+    const createdTopic = await topicApi.create(topic);
 
     return (dispatch) => {
       dispatch({
         type: "topic/create",
-        payload: newTopic,
+        payload: createdTopic,
       });
       dispatch({
         type: "meeting/createTopic",
         payload: {
-          _id: newTopic.meeting_id,
-          topicId: newTopic._id,
+          _id: createdTopic.meeting_id,
+          topicId: createdTopic._id,
         },
       });
     };
   };
 };
 
-export const updateTopic = (id, updates) => {
+export const update = (id, updates) => {
   return async function update() {
     const updatedTopic = await topicApi.update(id, updates);
 
@@ -62,18 +100,11 @@ export const updateTopic = (id, updates) => {
         type: "topic/update",
         payload: updatedTopic,
       });
-      dispatch({
-        type: "meeting/updateTopic",
-        payload: {
-          _id: updatedTopic.meeting_id,
-          topicId: updatedTopic._id,
-        },
-      });
     };
   };
 };
 
-export const deleteTopic = (topic) => {
+export const destroy = (topic) => {
   return async function destroy() {
     await topicApi.destroy(topic._id);
 
@@ -103,7 +134,7 @@ export const getMeetingTopics = (meeting_id) => {
         payload: { topics },
       });
       dispatch({
-        type: "meeting/updateTopics",
+        type: "meeting/setTopics",
         payload: {
           _id: topics[0].meeting_id,
           topicsIds: topics.map((topic) => topic._id),
