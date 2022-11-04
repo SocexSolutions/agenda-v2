ff92793bb5bca97402c65f86383f7ee481442e98
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-11-04 12:14:49 -0400

    feat(www:meet): add ability to close topics

--- www/components/pages/Meet/TopicDisplay/TopicDisplay.js
+++ www/components/pages/Meet/TopicDisplay/TopicDisplay.js
@@ -1,8 +1,15 @@
 import styles from "./TopicDisplay.module.scss";
 
+import { useDispatch } from "react-redux";
+
+import topicStore from "../../../../store/features/topic";
+
+import { Button } from "@mui/material";
 import { Fade } from "@mui/material";
 
 export default function TopicDisplay({ topic }) {
+  const dispatch = useDispatch();
+
   return (
     <Fade in={!!topic}>
       <div className={styles.topic_card}>
@@ -10,6 +17,15 @@ export default function TopicDisplay({ topic }) {
           <div className={styles.topic}>
             <h2 className={styles.topic_name}>{topic.name}</h2>
             <p className={styles.topic_description}>{topic.description}</p>
+            <Button
+              variant="contained"
+              disableElevation
+              onClick={() => {
+                dispatch(topicStore.actions.close(topic));
+              }}
+            >
+              Close
+            </Button>
           </div>
         )}
       </div>
