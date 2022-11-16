a5349d12c5eb7d34aa58a4192ed6c9fc9ac67d3e
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-11-16 18:45:57 -0500

    feat(api:invites): add invites model

---
+++ api/lib/models/invite.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const invite = new mongoose.Schema({
+  owner_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  invitee: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["open", "accepted", "rejected", "canceled"],
+    required: true,
+    default: "open",
+  },
+  group_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Group",
+  },
+});
+
+module.exports = mongoose.model("Invite", invite);
