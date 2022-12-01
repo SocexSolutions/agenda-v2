4eab95cd30e87e037d05d1f65e1a8947365f65b4
zbarnz <zbarnz@yahoo.com>
2022-11-30 21:27:00 -0700

    removed image upload functionality from frontend

--- www/components/Layout/ProfileButton/AccountModal/AccountModal.js
+++ www/components/Layout/ProfileButton/AccountModal/AccountModal.js
@@ -3,46 +3,17 @@ import { CameraAlt, Edit } from "@mui/icons-material";
 import styles from "./AccountModal.module.scss";
 import Link from "next/link";
 
+import { useState } from "react";
+
 import client from "../../../../api/client";
 
 import { userLogin } from "../../../../store/features/user";
 import { notify } from "../../../../store/features/snackbar";
 
 export default function AccountModal({ open, setOpen, user }) {
-  async function handleUpload(event) {
-    try {
-      console.log(event.target.files[0]);
-      const file = event.target.files[0];
-      const formData = new FormData();
+  
 
-      // Update the formData object
-      formData.append(
-        "imageFile",
-        file,
-        file.name
-      );
 
-      console.log(formData)
-
-      const res = await client.post(`/avatar`, formData);
-
-      console.log(res);
-    } catch (err) {
-      notify({
-        message: "Failed to fetch meeting: " + err.message,
-        type: "danger",
-      });
-    }
-  }
-
-  //this is temporary we will get this info from avatar
-  const getInitials = (username) =>
-    username
-      .match(/(\b\S)?/g)
-      .join("")
-      .match(/(^\S|\S$)?/g)
-      .join("")
-      .toUpperCase();
 
   return (
     <Modal open={open} onClose={() => setOpen(false)}>
@@ -53,13 +24,13 @@ export default function AccountModal({ open, setOpen, user }) {
               sx={{ bgcolor: "#009688", width: 100, height: 100 }}
               className={styles.avatar_current}
             >
-              {getInitials(user.username)}
-              <input
+              {}
+              {/* <input              //Will need this for image upload in future 
                 type="file"
                 className={styles.file_upload}
                 accept="image/*"
                 onChange={handleUpload}
-              />
+              /> */}
             </Avatar>
             <Avatar
               className={styles.avatar_change}
